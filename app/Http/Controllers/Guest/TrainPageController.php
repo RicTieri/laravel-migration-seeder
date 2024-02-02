5add735ec0e42accf7130fddcb5aeff86efd9ee0
RicTieri <tieri.riccardo@gmail.com>
2024-02-02 15:33:47 +0100

    added model, controller and migration for train

---
+++ app/Http/Controllers/Guest/TrainPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainPageController extends Controller
+{
+    public function index()
+    {
+        $trains = Train::all();
+        return view('pages.guest.trains.index', compact($trains));
+    }
+}
